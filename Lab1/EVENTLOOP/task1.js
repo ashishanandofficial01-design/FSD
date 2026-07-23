d7a21e74bd3ba6f1a3c237497247f9f010250555
ashishanandofficial01-ctrl <ashishanandofficial01@gmail.com>
2026-07-23 15:34:24 +0530

    task1

--- Lab1/EVENTLOOP/task1.js
+++ Lab1/EVENTLOOP/task1.js
@@ -1,20 +1,30 @@
-const f1=()=>{
-    console.log("f1");
-    
+const f1 = () => {
+  console.log("f1 starts");
+  f2();
+  console.log("f1 running");
+  console.log("f1 end");
 };
-const f2=()=>{
-    console.log("f2");
-    
+const f2 = () => {
+  console.log("f2 starts");
+  f3();
+  console.log("f2 running");
+  console.log("f2 ends");
 };
-const f3=()=>{
-    console.log("f3");
-    
+const f3 = () => {
+  console.log("f3 starts");
+  console.log("f3 running");
+  console.log("f3 end");
 };
 function main() {
-    console.log("main");
-    f1();
-    f2();
-    f3();
-    console.log("end main");
+  console.log("main");
+  f1();
+  console.log("end main");
 }
 main();
+//javascipt is single threaded and synchronous
+//in asynchronous we use event loop to manage the call stack
+//asynchronous call using timers
+//1. set timeout
+// 2. setImmediate
+// 3. process.nextTick
+// 4. setInterval
